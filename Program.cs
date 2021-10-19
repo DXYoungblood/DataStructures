d70a9b2d44f337a85f02769ab35e2026d9a085bb
Haidh58 <78934546+Haidh58@users.noreply.github.com>
2021-10-19 13:24:47 -0700

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,12 +1,88 @@
-﻿using System;
+using System;
 
-namespace CrackingTheCodeLabWeek4
+namespace ConsoleApp1
 {
+    class node
+    {
+        int value;
+        public node next;
+
+        public node(int Value)
+        {
+            value = Value;
+            next = null;
+        }
+
+        public int getValue()
+        {
+            return value;
+        }
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int[] a = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            
+        }
+    }
+
+    class linkedlist<T>
+    {
+        node head;
+
+        public linkedlist()
+        {
+            head = null;
+        }
+
+        public bool isEmpty()
+        {
+            return head == null;
+        }
+
+        public void addFirst(int x)
+        {
+            node finger = new node(x);
+
+            if (isEmpty())
+            {
+                head = finger;
+            }
+            else
+            {
+                finger.next = head;
+                head = finger;
+            }
+        }
+       
+        public void deleteFirst()
+        {
+            if (!isEmpty())
+            {
+                head = head.next;
+            }
+        }
+
+        public void print()
+        {
+            node finger = head;
+
+            string s = "";
+            while (finger != null)
+            {
+                s += finger.getValue() + " ";
+                finger = finger.next;
+            }
+
+            Console.WriteLine(s);
+        }
+
+        public void removeDups()
+        {
+
         }
     }
 }
+
